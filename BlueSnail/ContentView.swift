666eb53fe2c01c3a3b4c39925e50812b0dd50aaa
Anastasia <anastasia.tanczak@beecoming.fr>
2021-01-24 11:30:00 +0100

    Initial Commit

---
+++ BlueSnail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BlueSnail
+//
+//  Created by Anastasiia Tanczak on 24/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
