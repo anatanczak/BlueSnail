6b47aaa60c2ccab5ba82cf429461fce09ae4a1ac
anaviktoriv <anaviktoriv@gmail.com>
2021-01-28 11:12:13 +0100

    add jua font + textfield + next button

--- BlueSnail/Views/Onbording/FirstLaunchView.swift
+++ BlueSnail/Views/Onbording/FirstLaunchView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct FirstLaunchView: View {
+    @State private var nameInput: String = ""
+    
     var body: some View {
         GeometryReader {geometry in
             VStack(alignment: .center, spacing: 0, content: {
@@ -16,16 +18,29 @@ struct FirstLaunchView: View {
                     .clipped()
                     .scaledToFit()
                     .frame(minWidth: 0, maxWidth: .infinity, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .background(Color.blue)
                     .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
 
-                Text("BlaBla")
-                Text("BlaBla")
-                Text("BlaBla")
-                Spacer()
+                Text("What's your name?")
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20, content: {
+                    Spacer()
+                    Text("Create new account")
+                    Spacer()
+                    TextField("Name", text: $nameInput)
+                        .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+                        .background(Color("lightPink"))
+                        .cornerRadius(24)
+                    Spacer()
+                    Button(action: {
+                        print(nameInput)
+                    }, label: {
+                        Text("Next")
+                            .font(Font.custom("Jua-Regular", size: 50))
+                    })
+                    Spacer()
+                })
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
             })
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity ,alignment: .center)
-            .background(Color.yellow)
             .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
         }
 
